823934162fb28cc3134aabfc2704aca90c3ee522
SquaredR98 <yourfriendraviranjan@gmail.com>
2023-05-28 15:12:52 +0530

    Setup socket IO completed

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,7 @@ class Config {
   public SECRET_KEY_TWO : string | undefined;
   public CLIENT_URL : string | undefined;
   public NODE_ENV : string | undefined;
+  public REDIS_HOST : string | undefined;
 
   private readonly DEFAULT_DB_URI = 'mongodb://127.0.0.1:27017/test'
 
@@ -19,6 +20,7 @@ class Config {
     this.SECRET_KEY_TWO = process.env.SECRET_KEY_TWO || '';
     this.CLIENT_URL = process.env.CLIENT_URL;
     this.NODE_ENV = process.env.NODE_ENV;
+    this.REDIS_HOST = process.env.REDIS_HOST || '';
   }
 
   public validateConfig(): void {
