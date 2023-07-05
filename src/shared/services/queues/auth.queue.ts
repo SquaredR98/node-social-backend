9727b2467b68eb8d826f21e8abef83a30ba05f6c
SquaredR98 <yourfriendraviranjan@gmail.com>
2023-07-05 13:16:06 +0530

    Added the worker for auth and user job

--- src/shared/services/queues/auth.queue.ts
+++ src/shared/services/queues/auth.queue.ts
@@ -1,9 +1,11 @@
 import { BaseQueue } from '@services/queues/base.queue';
 import { IAuthJob } from '@auth/interfaces/auth.interface';
+import { authWorker } from '@services/workers/auth.worker';
 
 class AuthQueue extends BaseQueue {
   constructor() {
     super('auth');
+    this.processJob('addAuthUserToDB', 5, authWorker.addAuthUserToDB);
   }
 
   public addAuthUserJob (name: string, data: IAuthJob): void {
