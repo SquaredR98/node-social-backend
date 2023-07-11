4d76905f840412526c8a09728ea47fa32caf86c4
SquaredR98 <yourfriendraviranjan@gmail.com>
2023-07-11 16:22:27 +0530

    Restructured the Directory Structure

--- src/features/auth/controllers/current-user.ts
+++ src/features/auth/controllers/current-user.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { UserCache } from '@services/redis/user.cache';
-import { IUserDocument } from '../../user/interfaces/user.interface';
-import { userService } from '../../../shared/services/db/user.service';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { userService } from '@services/db/user.service';
 import HTTP_STATUS from 'http-status-codes';
 
 const userCache: UserCache = new UserCache();
