047407cb837cfa4e5d94a224cd9ae8557cc027ba
SquaredR98 <yourfriendraviranjan@gmail.com>
2023-07-11 13:53:58 +0530

    Completed the current user controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Application } from 'express';
 import { authRoutes } from '@auth/routes/authRoutes';
 import { serverAdapter } from '@services/queues/base.queue';
+import { currentUserRoute } from './features/auth/routes/currentRoutes';
 
 const BASE_PATH = '/api/v1';
 
@@ -9,6 +10,7 @@ export default (app: Application) => {
     app.use('/queues', serverAdapter.getRouter());
     app.use(BASE_PATH, authRoutes.routes());
     app.use(BASE_PATH, authRoutes.signOutRoutes());
+    app.use(BASE_PATH, currentUserRoute.routes());
     return;
   };
   routes();
