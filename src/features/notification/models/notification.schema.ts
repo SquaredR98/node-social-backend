eddf6abfef33ba651b23264fc997a793411cfcb0
SquaredR98 <yourfriendraviranjan@gmail.com>
2023-09-01 18:21:02 +0530

    Notification interface, model, and create method implemented

---
+++ src/features/notification/models/notification.schema.ts
@@ -0,0 +1,80 @@
+import Logger from 'bunyan';
+import mongoose, { Model, Schema, model } from 'mongoose';
+
+import { config } from '@root/config';
+import {
+  INotification,
+  INotificationDocument
+} from '@notification/interfaces/notification.interface';
+import { notificationService } from '@services/db/notification.service';
+
+const logger: Logger = config.createLogger('Notification Schema');
+
+const notificationSchema: Schema = new Schema(
+  {
+    userTo: { type: mongoose.Types.ObjectId, ref: 'User', index: true },
+    userFrom: { type: mongoose.Types.ObjectId, ref: 'User' },
+    read: { type: Boolean, default: false },
+    message: { type: String, default: '' },
+    notificationType: { type: String, enum: ['comment', 'followed'] },
+    entityItemId: { type: mongoose.Types.ObjectId },
+    createdItemId: { type: mongoose.Types.ObjectId },
+    comment: { type: String, default: '' },
+    reaction: { type: String, default: '' },
+    post: { type: String, default: '' },
+    imgId: { type: String, default: '' },
+    imgVersion: { type: String, default: '' },
+    gifUrl: { type: String, default: '' }
+  },
+  { timestamps: true }
+);
+
+notificationSchema.methods.insertNotification = async (data: INotification) => {
+  const {
+    userTo,
+    userFrom,
+    message,
+    notificationType,
+    entityId,
+    createdItemId,
+    comment,
+    reaction,
+    post,
+    imgId,
+    imgVersion,
+    gifUrl
+  } = data;
+
+  await NotificationModel.create({
+    userTo,
+    userFrom,
+    message,
+    notificationType,
+    entityId,
+    createdItemId,
+    comment,
+    reaction,
+    post,
+    imgId,
+    imgVersion,
+    gifUrl
+  });
+
+  try {
+    const notifications: INotificationDocument[] = await notificationService.getNotification(
+      userTo
+    );
+    return notifications;
+  } catch (error) {
+    logger.error(error);
+    return error;
+  }
+};
+
+const NotificationModel: Model<INotificationDocument> = model<INotificationDocument>(
+  'Notification',
+  notificationSchema,
+  'Notification'
+);
+
+export { NotificationModel };
